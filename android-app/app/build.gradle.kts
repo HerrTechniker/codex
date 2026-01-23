f3ad6efe3b3fe7304c33eaf629ecf0dc2cff778b
herrtechniker <83061806+HerrTechniker@users.noreply.github.com>
2026-01-23 15:45:31 +0100

    Add Compose foundation layout dependency

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -41,6 +41,7 @@ kotlin {
 dependencies {
   implementation("androidx.core:core-ktx:1.12.0")
   implementation("androidx.activity:activity-compose:1.8.2")
+  implementation("androidx.compose.foundation:foundation-layout:1.5.4")
   implementation("androidx.compose.ui:ui:1.5.4")
   implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
   implementation("androidx.compose.material3:material3:1.2.0")
