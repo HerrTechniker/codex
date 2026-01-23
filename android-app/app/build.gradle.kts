8b5d678f8a758352eb47cdf289455c69d81289bd
herrtechniker <83061806+HerrTechniker@users.noreply.github.com>
2026-01-23 15:49:54 +0100

    Add lifecycle ktx and coroutines deps

--- android-app/app/build.gradle.kts
+++ android-app/app/build.gradle.kts
@@ -46,10 +46,12 @@ dependencies {
   implementation("androidx.compose.ui:ui-tooling-preview:1.5.4")
   implementation("androidx.compose.material3:material3:1.2.0")
   implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
+  implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
   implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0")
   implementation("androidx.compose.material:material-icons-extended:1.5.4")
   implementation("androidx.datastore:datastore:1.0.0")
   implementation("androidx.datastore:datastore-preferences:1.0.0")
+  implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
   implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
   implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
 
